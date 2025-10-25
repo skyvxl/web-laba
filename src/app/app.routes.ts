f20b6778e2a0daa93724f3e886a10dfd6784bb0d
skyvxl <wivvz@yandex.ru>
2025-10-25 23:43:52 +0300

    Добавить компоненты main и not-found, настроить маршрутизацию; обновить шаблон app, index.html и .vscode/settings.json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./components/pages/main/main').then(m => m.Main)
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./components/pages/not-found/not-found').then(m => m.NotFound)
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('laba');
 }
