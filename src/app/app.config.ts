c3e80677bbfba4d4114e6b66ab0d4f06997bcf98
skyvxl <wivvz@yandex.ru>
2025-11-01 14:11:01 +0300

    feat(firebase): integrate Firebase services and update configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,31 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'test-angular-deploy-1b983',
+        appId: '1:52651766909:web:b6bfb7f3f19417e3a06d14',
+        storageBucket: 'test-angular-deploy-1b983.firebasestorage.app',
+        apiKey: 'AIzaSyAhM8cqT0Wa7TwMZCJXxieMF6dZ6WNtMtk',
+        authDomain: 'test-angular-deploy-1b983.firebaseapp.com',
+        messagingSenderId: '52651766909',
+        measurementId: 'G-KJYTJQVWWE',
+      }),
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideMessaging(() => getMessaging()),
   ],
 };
